c3b69a99f73579f4526e8b7e04eec448fd1f5f4f
alamostudios <octokid13@gmail.com>
2026-06-16 00:02:21 -0400

    Sandy Shores Station Locations
    Sandy Shores EMS by Redsaint
    Sandy Shores Fire by Prompt Mods

---
+++ [inferno-collection]/inferno-station-locations/sandy-shores-fire-dept-prompt.lua
@@ -0,0 +1,42 @@
+-- Inferno Collection Station Alert
+-- 
+-- Copyright (c) 2019-2026, Christopher M, Inferno Collection. All rights reserved.
+--
+-- Generated with the Inferno Station Alert Tool 06/01/2026 01:28:01 UTC
+
+location { name = "Sandy Shores Fire Station" }
+
+    light { position = vec3(1703.775, 3592.976, 37.376), rotation = vec3(0, 0, 29.968) }
+    light { position = vec3(1692.341, 3596.384, 38.654), rotation = vec3(0, 0, 120) }
+    light { position = vec3(1687.804, 3600.822, 37.505), rotation = vec3(0, 0, 30) }
+    light { position = vec3(1682.965, 3600.08, 37.814), rotation = vec3(0, 0, 120) }
+    light { position = vec3(1693.678, 3588.833, 37.716), rotation = vec3(0, 0, 30) }
+    light { position = vec3(1671.293, 3580.622, 37.656), rotation = vec3(0, 0, 120) }
+    light { position = vec3(1670.518, 3594.721, 37.552), rotation = vec3(0, 0, 30) }
+
+    bollardlight { position = vec3(1679.487, 3618.47, 35.254), rotation = vec3(0, 0, -84.288) }
+    bollardlight { position = vec3(1700.192, 3630.211, 34.734), rotation = vec3(0, 0, 113.205) }
+    bollardlight { position = vec3(1721.44, 3592.635, 34.999), rotation = vec3(0, 0, 32.852) }
+    bollardlight { position = vec3(1702.048, 3583.836, 35.3), rotation = vec3(0, 0, 88.189) }
+
+    door { name = "A4", position = vec3(1701.075, 3592.691, 37.968), archetype = "escobar_sandy_fd_int_garage_gate"}
+    door { name = "A3", position = vec3(1705.286, 3595.112, 37.967), archetype = "escobar_sandy_fd_int_garage_gate"}
+    door { name = "A2", position = vec3(1709.519, 3597.576, 37.966), archetype = "escobar_sandy_fd_int_garage_gate"}
+    door { name = "A1", position = vec3(1713.966, 3599.37, 37.546), archetype = "escobar_sandy_fd_int_garage_gate"}
+    door { name = "B4", position = vec3(1700.043, 3623.497, 37.256), archetype = "escobar_sandy_fd_int_garage_gate"}
+    door { name = "B3", position = vec3(1696.363, 3620.373, 37.979), archetype = "escobar_sandy_fd_int_garage_gate"}
+    door { name = "B2", position = vec3(1691.685, 3618.68, 37.253), archetype = "escobar_sandy_fd_int_garage_gate"}
+    door { name = "B1", position = vec3(1687.649, 3615.952, 37.684), archetype = "escobar_sandy_fd_int_garage_gate"}
+
+    doorcontrols { position = vec3(1700.833, 3607.753, 35.821), rotation = vec3(0, 0, 30) }
+
+    ceilingspeaker { position = vec3(1689.263, 3595.079, 38.691), rotation = vec3(0, 0.002, 0) }
+    ceilingspeaker { position = vec3(1693.495, 3585.551, 38.691), rotation = vec3(0, 0.002, 0) }
+    ceilingspeaker { position = vec3(1680.371, 3582.327, 38.691), rotation = vec3(0, 0.002, 0) }
+    ceilingspeaker { position = vec3(1667.825, 3581.031, 38.691), rotation = vec3(0, 0.002, 0) }
+    ceilingspeaker { position = vec3(1670.85, 3594.057, 38.691), rotation = vec3(0, 0.002, 0) }
+
+    computer { position = vec3(1683.066, 3580.479, 35.633), archetype = "escobar_sandy_fd_int_reception_desk" }
+
+    callscreen { position = vec3(1694.856, 3583.078, 38.299), rotation = vec3(0, 0, -150) }
+    callscreen { position = vec3(1686.116, 3595.035, 38.549), rotation = vec3(0, 0, 120) }
